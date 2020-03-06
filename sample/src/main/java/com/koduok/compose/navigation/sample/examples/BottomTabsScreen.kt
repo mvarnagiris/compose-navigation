384354eb21f3ceae90b3051deff0f2e541e2693d
Mantas <mantas.varnagiris@gmail.com>
2020-03-06 12:07:43 +0200

    Updated to dev06

--- sample/src/main/java/com/koduok/compose/navigation/sample/examples/BottomTabsScreen.kt
+++ sample/src/main/java/com/koduok/compose/navigation/sample/examples/BottomTabsScreen.kt
@@ -4,17 +4,12 @@ import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.foundation.Box
 import androidx.ui.foundation.Clickable
-import androidx.ui.foundation.DrawBackground
 import androidx.ui.graphics.Color
 import androidx.ui.layout.Center
 import androidx.ui.layout.Column
-import androidx.ui.layout.LayoutHeight
-import androidx.ui.layout.Row
 import androidx.ui.layout.RowScope
+import androidx.ui.material.BottomNavigation
 import androidx.ui.material.MaterialTheme
-import androidx.ui.text.font.FontWeight
-import androidx.ui.text.font.FontWeight.Companion
-import androidx.ui.unit.dp
 import com.koduok.compose.navigation.Router
 import com.koduok.compose.navigation.core.BackStack
 import com.koduok.compose.navigation.sample.examples.TabRoute.TabA
@@ -33,7 +28,7 @@ fun BottomTabsScreen() {
                 }
             }
 
-            Row(modifier = LayoutHeight(72.dp) + DrawBackground(color = Color.Gray)) {
+            BottomNavigation {
                 TabButton(backStack = this@Router, tabRoute = TabA, currentTabRoute = it.data)
                 TabButton(backStack = this@Router, tabRoute = TabB, currentTabRoute = it.data)
                 TabButton(backStack = this@Router, tabRoute = TabC, currentTabRoute = it.data)
@@ -44,12 +39,13 @@ fun BottomTabsScreen() {
 
 @Composable
 fun RowScope.TabButton(backStack: BackStack<TabRoute>, tabRoute: TabRoute, currentTabRoute: TabRoute) {
-    Box(modifier = LayoutFlexible(1f)) {
+    val isSelected = currentTabRoute == tabRoute
+    Box(modifier = LayoutFlexible(1f), backgroundColor = Color.Gray.copy(alpha = if (isSelected) 1f else 0.3f)) {
         Clickable(onClick = { backStack.replace(tabRoute) }) {
             Center {
                 Text(
                     text = tabRoute.toString(),
-                    style = MaterialTheme.typography().h5.copy(fontWeight = if (currentTabRoute == tabRoute) FontWeight.Bold else Companion.Normal)
+                    style = MaterialTheme.typography().h5
                 )
             }
         }
@@ -58,14 +54,14 @@ fun RowScope.TabButton(backStack: BackStack<TabRoute>, tabRoute: TabRoute, curre
 
 sealed class TabRoute {
     object TabA : TabRoute() {
-        override fun toString(): String = "TabA"
+        override fun toString(): String = "A"
     }
 
     object TabB : TabRoute() {
-        override fun toString(): String = "TabB"
+        override fun toString(): String = "B"
     }
 
     object TabC : TabRoute() {
-        override fun toString(): String = "TabC"
+        override fun toString(): String = "C"
     }
 }
